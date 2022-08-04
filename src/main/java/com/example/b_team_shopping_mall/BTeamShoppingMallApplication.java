db082a32335e66446c448feec5d52c901c3154a5
CordHouse <qkqktl5310@gmail.com>
2022-08-04 20:22:52 +0900

    feat : Create Project

---
+++ src/main/java/com/example/b_team_shopping_mall/BTeamShoppingMallApplication.java
@@ -0,0 +1,13 @@
+package com.example.b_team_shopping_mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BTeamShoppingMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BTeamShoppingMallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/b_team_shopping_mall/BTeamShoppingMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.b_team_shopping_mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BTeamShoppingMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
