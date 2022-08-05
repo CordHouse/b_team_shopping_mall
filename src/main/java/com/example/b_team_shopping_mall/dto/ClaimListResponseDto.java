55b0217d42c43ff8945ef8a563ac1d16c6de2e07
chrkb1569 <chrkb1569@naver.com>
2022-08-05 16:53:15 +0900

    feat : Add Dto Files

---
+++ src/main/java/com/example/b_team_shopping_mall/dto/ClaimListResponseDto.java
@@ -0,0 +1,19 @@
+package com.example.b_team_shopping_mall.dto;
+
+import com.example.b_team_shopping_mall.entity.Claim;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+public class ClaimListResponseDto {
+    private String title;
+    private String content;
+    private String writer;
+
+    public ClaimListResponseDto toDto(Claim claim) {
+        return new ClaimListResponseDto(claim.getTitle(), claim.getContent(), claim.getWriter());
+    }
+}
