82b2fe10feff1de4846685ea8048a1cfb8c3b788
CordHouse <qkqktl5310@gmail.com>
2022-08-05 15:18:45 +0900

    refactor : Add Exclude -> SecurityAutoConfiguration.class

--- src/main/java/com/example/b_team_shopping_mall/BTeamShoppingMallApplication.java
+++ src/main/java/com/example/b_team_shopping_mall/BTeamShoppingMallApplication.java
@@ -2,8 +2,9 @@ package com.example.b_team_shopping_mall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class) // security 일시적 제한
 public class BTeamShoppingMallApplication {
 
 	public static void main(String[] args) {
