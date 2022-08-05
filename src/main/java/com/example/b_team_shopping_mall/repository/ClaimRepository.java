ede12e155a0d5953d0b293b3327d2f09b0311f31
chrkb1569 <chrkb1569@naver.com>
2022-08-05 16:53:52 +0900

    feat : Add Repository

---
+++ src/main/java/com/example/b_team_shopping_mall/repository/ClaimRepository.java
@@ -0,0 +1,8 @@
+package com.example.b_team_shopping_mall.repository;
+
+
+import com.example.b_team_shopping_mall.entity.Claim;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClaimRepository extends JpaRepository<Claim, Long> {
+}
