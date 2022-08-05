7ed7afedc204e0d069101395f6de193b5aa2e0d1
CordHouse <qkqktl5310@gmail.com>
2022-08-05 15:24:07 +0900

    feat : Add Register API

--- src/main/java/com/example/b_team_shopping_mall/service/RegisterService.java
+++ src/main/java/com/example/b_team_shopping_mall/service/RegisterService.java
@@ -13,6 +13,7 @@ import java.util.List;
 public class RegisterService {
     private final RegisterRepository registerRepository;
 
+    // 회원가입 함수
     @Transactional
     public Register signUp(Register register){
         Register register1 = new Register(register.getUsername(), register.getUserid(), register.getUserpw(), register.getUseremail());
@@ -20,6 +21,7 @@ public class RegisterService {
         return register1;
     }
 
+    // 로그인 함수
     @Transactional
     public String login(Register register){
         List<Register> registerList = registerRepository.findAll();
