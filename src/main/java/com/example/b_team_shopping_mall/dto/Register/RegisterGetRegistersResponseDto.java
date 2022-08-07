152c0b99664552324f662b68deb4d7f5f242b08f
CordHouse <qkqktl5310@gmail.com>
2022-08-08 02:05:11 +0900

    feat : Add RegisterGetRegistersDto

---
+++ src/main/java/com/example/b_team_shopping_mall/dto/Register/RegisterGetRegistersResponseDto.java
@@ -0,0 +1,20 @@
+package com.example.b_team_shopping_mall.dto.Register;
+
+import com.example.b_team_shopping_mall.entity.Register;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterGetRegistersResponseDto {
+    private Long id;
+    private String membername;
+
+    public RegisterGetRegistersResponseDto toDto(Register register){
+        return new RegisterGetRegistersResponseDto(register.getId(), register.getMembername());
+    }
+}
