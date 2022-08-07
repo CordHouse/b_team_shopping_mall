97b79011fffa362aa0030939f2a996fbf72763a0
CordHouse <qkqktl5310@gmail.com>
2022-08-08 02:39:18 +0900

    style : Change package

---
+++ src/main/java/com/example/b_team_shopping_mall/dto/Claim/ClaimCreateResponseDto.java
@@ -0,0 +1,19 @@
+package com.example.b_team_shopping_mall.dto.Claim;
+
+import com.example.b_team_shopping_mall.entity.Claim;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClaimCreateResponseDto {
+    private String title;
+    private String content;
+    private String writer;
+
+    public ClaimCreateResponseDto toDto(Claim claim) {
+        return new ClaimCreateResponseDto(claim.getTitle(), claim.getContent(), claim.getRegister().getMembername());
+    }
+}
--- src/main/java/com/example/b_team_shopping_mall/dto/Claim/ClaimListResponseDto.java
+++ src/main/java/com/example/b_team_shopping_mall/dto/Claim/ClaimListResponseDto.java
@@ -1,19 +1,19 @@
-package com.example.b_team_shopping_mall.dto;
+package com.example.b_team_shopping_mall.dto.Claim;
 
 import com.example.b_team_shopping_mall.entity.Claim;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
 @AllArgsConstructor
+@Data
 @NoArgsConstructor
-public class ClaimCreateResponseDto {
+public class ClaimListResponseDto {
     private String title;
     private String content;
     private String writer;
 
-    public ClaimCreateResponseDto toDto(Claim claim) {
-        return new ClaimCreateResponseDto(claim.getTitle(), claim.getContent(), claim.getWriter());
+    public ClaimListResponseDto toDto(Claim claim) {
+        return new ClaimListResponseDto(claim.getTitle(), claim.getContent(), claim.getRegister().getMembername());
     }
 }
